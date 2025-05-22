2d51c4cf8c9493547ee7034b8664417ccf00fb0d
knapptan <nikitas@live.ru>
2025-05-22 22:29:41 +0300

    init mod and fix storage interface

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Knapptan/Go_T1_Name_info_rest
+
+go 1.24.1
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,11 +1,15 @@
 package storage
 
-import "context"
+import (
+	"context"
+
+	"github.com/Knapptan/Go_T1_Name_info_rest/internal/models"
+)
 
 type QuotesRepository interface {
-	CreateQuote(ctx context.Context) error
-	GetQuote(ctx context.Context) error
-	GetAllQuote(ctx context.Context) error
-	UpdateQuote(ctx context.Context) error
-	DeleteQuote(ctx context.Context) error
+	Create(ctx context.Context, quote models.CreateQuoteRequest) (string, error)
+	GetAll(ctx context.Context) ([]models.Quote, error)
+	GetRandom(ctx context.Context) (models.Quote, error)
+	GetByAuthor(ctx context.Context, author string) ([]models.Quote, error)
+	Delete(ctx context.Context, id string) error
 }

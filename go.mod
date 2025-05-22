08676b83a6bac137016aad22a06a416cc1b5d881
knapptan <nikitas@live.ru>
2025-05-22 22:37:42 +0300

    add inmamory storage model and Create method

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Knapptan/Go_T1_Name_info_rest
 
 go 1.24.1
+
+require github.com/google/uuid v1.6.0
--- internal/storage/inmemory.go
+++ internal/storage/inmemory.go
@@ -1,1 +1,28 @@
 package storage
+
+import (
+	"context"
+	"sync"
+
+	"github.com/Knapptan/Go_T1_Name_info_rest/internal/models"
+	"github.com/google/uuid"
+)
+
+type InMemoryStorage struct {
+	quotes map[string]models.Quote
+	mu     sync.RWMutex
+}
+
+func (s *InMemoryStorage) Create(ctx context.Context, quoteReq models.CreateQuoteRequest) (string, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	id := uuid.New().String()
+	quote := models.Quote{
+		ID:     id,
+		Author: quoteReq.Author,
+		Text:   quoteReq.Text,
+	}
+	s.quotes[id] = quote
+	return id, nil
+}

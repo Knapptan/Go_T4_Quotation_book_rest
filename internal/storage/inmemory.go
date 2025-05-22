15dd2a8c60668a6309a44cb3f3f755a05cc28495
knapptan <nikitas@live.ru>
2025-05-22 22:53:41 +0300

    add all storage methods

--- internal/storage/inmemory.go
+++ internal/storage/inmemory.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
 	"github.com/Knapptan/Go_T1_Name_info_rest/internal/models"
@@ -25,4 +26,52 @@ func (s *InMemoryStorage) Create(ctx context.Context, quoteReq models.CreateQuot
 	}
 	s.quotes[id] = quote
 	return id, nil
+}
+
+func (s *InMemoryStorage) GetAll(ctx context.Context) ([]models.Quote, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	quotes := make([]models.Quote, 0, len(s.quotes))
+	for _, q := range s.quotes {
+		quotes = append(quotes, q)
+	}
+	return quotes, nil
+}
+
+func (s *InMemoryStorage) GetRandom(ctx context.Context) (models.Quote, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	var quote models.Quote
+	for _, q := range s.quotes {
+		quote = q
+		break
+	}
+	return quote, nil
+}
+
+func (s *InMemoryStorage) GetByAuthor(ctx context.Context, author string) ([]models.Quote, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	quotes := make([]models.Quote, 0, 1)
+	for _, q := range s.quotes {
+		if q.Author == author {
+			quotes = append(quotes, q)
+		}
+	}
+	return quotes, nil
+}
+
+func (s *InMemoryStorage) Delete(ctx context.Context, id string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if _, exists := s.quotes[id]; !exists {
+		return fmt.Errorf("quote with id %q not found", id)
+	}
+
+	delete(s.quotes, id)
+	return nil
 }
